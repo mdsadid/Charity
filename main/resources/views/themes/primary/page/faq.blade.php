49007a046397d58a100c3a7d6274bb397d304fdd
Sadid Hasan Rakib <sadid.hasan14@gmail.com>
2024-03-20 11:24:13 +0600

    change foreach loop to forelse loop

--- main/resources/views/themes/primary/page/faq.blade.php
+++ main/resources/views/themes/primary/page/faq.blade.php
@@ -14,24 +14,22 @@
             <div class="accordion custom--accordion" id="accordionExample">
                 <div class="row g-4 align-items-center justify-content-center">
                     <div class="col-lg-8 col-md-10">
-                        @if (count($faqElements))
-                            @foreach ($faqElements as $faq)
-                                <div class="accordion-item" data-aos="fade-up" data-aos-duration="1500">
-                                    <h2 class="accordion-header">
-                                        <button class="accordion-button @if (!$loop->first) collapsed @endif" type="button" data-bs-toggle="collapse" data-bs-target="{{ '#collapse_' . $loop->iteration }}" aria-expanded="{{ $loop->first ? 'true' : 'false' }}" aria-controls="{{ 'collapse_' . $loop->iteration }}">
-                                            {{ __(@$faq->data_info->question) }}
-                                        </button>
-                                    </h2>
-                                    <div id="{{ 'collapse_' . $loop->iteration }}" class="accordion-collapse collapse @if ($loop->first) show @endif" data-bs-parent="#accordionExample">
-                                        <div class="accordion-body">
-                                            @php echo @$faq->data_info->answer @endphp
-                                        </div>
+                        @forelse ($faqElements as $faq)
+                            <div class="accordion-item" data-aos="fade-up" data-aos-duration="1500">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button @if (!$loop->first) collapsed @endif" type="button" data-bs-toggle="collapse" data-bs-target="{{ '#collapse_' . $loop->iteration }}" aria-expanded="{{ $loop->first ? 'true' : 'false' }}" aria-controls="{{ 'collapse_' . $loop->iteration }}">
+                                        {{ __(@$faq->data_info->question) }}
+                                    </button>
+                                </h2>
+                                <div id="{{ 'collapse_' . $loop->iteration }}" class="accordion-collapse collapse @if ($loop->first) show @endif" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        @php echo @$faq->data_info->answer @endphp
                                     </div>
                                 </div>
-                            @endforeach
-                        @else
+                            </div>
+                        @empty
                             <p class="text-center" data-aos="fade-up" data-aos-duration="1500">{{ __($emptyMessage) }}</p>
-                        @endif
+                        @endforelse
                     </div>
                 </div>
             </div>
